5b77ba2306101995ba2c5967e4bdd24a334e41ad
419360214@qq.com <130681li>
2019-07-12 13:34:41 +0800

    更新h5导航参数

--- kgaodelib/src/main/java/com/mo/kgaodelib/GdUtil.java
+++ kgaodelib/src/main/java/com/mo/kgaodelib/GdUtil.java
@@ -1,7 +1,6 @@
 package com.mo.kgaodelib;
 
 import android.content.Context;
-import android.view.View;
 
 import com.amap.api.maps.model.Poi;
 import com.amap.api.navi.AmapNaviPage;
@@ -9,7 +8,6 @@ import com.amap.api.navi.AmapNaviParams;
 import com.amap.api.navi.AmapNaviType;
 import com.amap.api.navi.AmapPageType;
 import com.amap.api.navi.INaviInfoCallback;
-import com.amap.api.navi.model.AMapNaviLocation;
 
 /*
  * 作者：M
@@ -20,86 +18,25 @@ import com.amap.api.navi.model.AMapNaviLocation;
 public class GdUtil {
 
     /**
-     * 高德导航
-     * 传入终点经纬度
-     * 原先是传 NearBuildBeans.DataBean commentData
-     * 为了方便封出去库的时候就传这个 double endlatiude,double endlongitude
+     * 跳高德导航（跳进的是高德的h5导航页面）
+     *
+     * @param context      上下文
+     * @param endlatitude  终点纬度
+     * @param endlongitude 终点经度
+     * @param callback     回调，可null
      */
-    public static void startAMapNavigational(Context context, double endlatiude, double endlongitude) {
+    public static void startAMapNavigational(Context context, double endlatitude, double endlongitude, INaviInfoCallback callback) {
         //传入起点经纬度
         com.amap.api.maps.model.LatLng startLatLng = new com.amap.api.maps.model.LatLng(GdConstant.latitude, GdConstant.longitude);
         //传入终点经纬度
-        com.amap.api.maps.model.LatLng endLatLng = new com.amap.api.maps.model.LatLng(endlatiude, endlongitude);
+        com.amap.api.maps.model.LatLng endLatLng = new com.amap.api.maps.model.LatLng(endlatitude, endlongitude);
         //起点
         Poi start = new Poi("起点", startLatLng, "");
         //终点
         Poi end = new Poi("终点", endLatLng, "");
         AmapNaviParams amapNaviParams = new AmapNaviParams(start, null, end, AmapNaviType.DRIVER, AmapPageType.NAVI);
         amapNaviParams.setUseInnerVoice(true);
-        AmapNaviPage.getInstance().showRouteActivity(context, amapNaviParams, new INaviInfoCallback() {
-            @Override
-            public void onInitNaviFailure() {
-            }
-
-            @Override
-            public void onGetNavigationText(String s) {
-            }
-
-            @Override
-            public void onLocationChange(AMapNaviLocation aMapNaviLocation) {
-            }
-
-            @Override
-            public void onArriveDestination(boolean b) {
-            }
-
-            @Override
-            public void onStartNavi(int i) {
-            }
-
-            @Override
-            public void onCalculateRouteSuccess(int[] ints) {
-            }
-
-            @Override
-            public void onCalculateRouteFailure(int i) {
-            }
-
-            @Override
-            public void onStopSpeaking() {
-            }
-
-            @Override
-            public void onReCalculateRoute(int i) {
-            }
-
-            @Override
-            public void onExitPage(int i) {
-            }
-
-            @Override
-            public void onStrategyChanged(int i) {
-            }
-
-            @Override
-            public View getCustomNaviBottomView() {
-                return null;
-            }
-
-            @Override
-            public View getCustomNaviView() {
-                return null;
-            }
-
-            @Override
-            public void onArrivedWayPoint(int i) {
-            }
-
-            @Override
-            public void onMapTypeChanged(int i) {
-
-            }
-        });
+        AmapNaviPage.getInstance().showRouteActivity(context, amapNaviParams, callback);
     }
 
 }
