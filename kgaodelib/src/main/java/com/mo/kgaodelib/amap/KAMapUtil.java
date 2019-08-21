70c10eed2e1a5959c6f4adeae437150843333a6a
419360214@qq.com <130681li>
2019-08-21 14:35:49 +0800

    更新h5导航参数

--- kgaodelib/src/main/java/com/mo/kgaodelib/amap/KAMapUtil.java
+++ kgaodelib/src/main/java/com/mo/kgaodelib/amap/KAMapUtil.java
@@ -4,6 +4,7 @@ import com.amap.api.maps.AMap;
 import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.MyLocationStyle;
 
 /**
  * @ author：mo
@@ -19,8 +20,14 @@ public class KAMapUtil {
         setLocationStyle();
     }
 
-    private void setLocationStyle() {
-
+    public void setLocationStyle() {
+        // 是否显示定位按钮
+        MyLocationStyle myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+//        myLocationStyle.showMyLocation(true);
+        // 默认模式，连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动，1秒1次定位
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
     }
 
     /**
